9dc4b219a78c687edc52dae9d7632c4a4a83a6be
Anna Malik <anka98@op.pl>
2020-03-06 08:16:00 +0100

    base application

---
+++ src/main/java/com/openx/openxtask/OpenxTaskApplication.java
@@ -0,0 +1,13 @@
+package com.openx.openxtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenxTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenxTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/openx/openxtask/OpenxTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openx.openxtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenxTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
